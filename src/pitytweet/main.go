e16d46daf2a01a97b2f57fcdef3df584af6bc896
iknite <enrique@iknite.com>
2016-02-02 09:42:31 +0100

    Add connection to twitter via Oauth1.0

--- src/pitytweet/main.go
+++ src/pitytweet/main.go
@@ -3,18 +3,71 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/mrjones/oauth"
+	"io/ioutil"
+	// "golang.org/x/net/context"
+	// "golang.org/x/oauth2"
+	"log"
+	"os"
+	"os/exec"
 )
 
 func main() {
-	var screen_name *string = flag.String("name", "", "twitter user name")
-	var consumer_key *string = flag.String("key", "", "")
-	var consumer_secret *string = flag.String("secret", "", "")
+	var screenName *string = flag.String("name", "", "twitter user name")
+	var consumerKey *string = flag.String("key", "", "")
+	var consumerSecret *string = flag.String("secret", "", "")
 
 	flag.Parse()
 
-	fmt.Printf("public flags: name %s \n", *screen_name)
-	if *consumer_key != "" && *consumer_secret != "" {
-		fmt.Println("Credentials recieved")
+	fmt.Printf("fetching for: %s \n", *screenName)
+	if *consumerKey == "" || *consumerSecret == "" {
+		// panic("needed key and secret, exiting")
+		log.Fatal("needed key and secret, exiting")
+		os.Exit(1)
 	}
+
+	c := oauth.NewConsumer(
+		*consumerKey,
+		*consumerSecret,
+		oauth.ServiceProvider{
+			RequestTokenUrl:   "https://api.twitter.com/oauth/request_token",
+			AuthorizeTokenUrl: "https://api.twitter.com/oauth/authorize",
+			AccessTokenUrl:    "https://api.twitter.com/oauth/access_token",
+		})
+
+	c.Debug(true)
+
+	requestToken, u, err := c.GetRequestTokenAndUrl("oob")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	exec.Command("google-chrome-stable", u).Run()
+
+	fmt.Println("(1) Grant access, you should get back a verification code.")
+	fmt.Println("(2) Enter that verification code here: ")
+
+	verificationCode := ""
+	fmt.Scanln(&verificationCode)
+
+	accessToken, err := c.AuthorizeToken(requestToken, verificationCode)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client, err := c.MakeHttpClient(accessToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	response, err := client.Get(
+		"https://api.twitter.com/1.1/statuses/home_timeline.json?count=1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+
+	bits, err := ioutil.ReadAll(response.Body)
+	fmt.Println("The newest item in your home timeline is: " + string(bits))
 
 }
